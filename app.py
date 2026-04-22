b717b3767d607b81af57124eb1d781ccdcf97738
mpraveen2302-dotcom <mpraveen2302@gmail.com>
2026-04-22 17:50:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import json
-import random
+from tensorflow.keras.models import load_model
+
+# Load model
+model = load_model("model.h5")
 
 # Load class names
 with open("class_names.json", "r") as f:
@@ -16,9 +19,15 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
-    # Fake prediction (for deployment demo)
-    predicted_class = random.choice(class_names)
-    confidence = random.uniform(85, 99)
+    # Preprocess (MUST match training)
+    img = image.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Predict
+    prediction = model.predict(img_array)
+    predicted_class = class_names[np.argmax(prediction)]
+    confidence = np.max(prediction) * 100
 
     st.success(f"Prediction: {predicted_class}")
     st.info(f"Confidence: {confidence:.2f}%")
