225f60f9f01484d19fb36d775c4e5e6f6b130881
mpraveen2302-dotcom <mpraveen2302@gmail.com>
2026-04-26 11:06:21 +0530

    Create tflite_model.py

---
+++ tflite_model.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class SimpleTFLite:
+    def __init__(self, model_path):
+        import tflite_runtime.interpreter as tflite
+        self.interpreter = tflite.Interpreter(model_path=model_path)
+        self.interpreter.allocate_tensors()
+
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+
+    def predict(self, img):
+        self.interpreter.set_tensor(self.input_details[0]['index'], img)
+        self.interpreter.invoke()
+        return self.interpreter.get_tensor(self.output_details[0]['index'])
