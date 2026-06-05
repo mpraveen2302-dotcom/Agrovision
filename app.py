e2fcedd9a1f71196cefba39049f20ce7b5bb7241
mpraveen2302-dotcom <mpraveen2302@gmail.com>
2026-06-05 17:47:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -432,7 +432,7 @@ with c4:
 def load_model():
     try:
         model = tf.keras.models.load_model(
-            "agrovision_model_final.h5",
+            "model.tflite",
             compile=False
         )
 
